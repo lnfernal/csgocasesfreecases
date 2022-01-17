7a11d198428674736dc831ca0d9cb41ff4aba51c
motzmartin <94078957+motzmartin@users.noreply.github.com>
2022-01-17 16:43:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,30 +3,28 @@ const https = require('https');
 const token = 'hz7z9bvx8xcsko8sko00s0wo0kcs80k';
 
 function opencase(id) {
+    const url = `https://csgocases.com/api.php/case/open?id=${id}&number=1&test=false`;
     const options = {
-        host: 'csgocases.com',
-        port: 443,
-        path: `/api.php/case/open?id=${id}&number=1&test=false`,
-        method: 'GET',
         headers: {
             'cookie': `sfRemember=${token}`
         }
     };
-    const req = https.request(options, (res) => {
-        res.setEncoding('utf8');
+    https.get(url, options, (res) => {
+        let data = '';
         res.on('data', (d) => {
-            console.log(d);
+            data += d;
         });
-    });
-    req.on('error', (e) => {
+        res.on('end', () => {
+            console.log(JSON.parse(data));
+        });
+    }).on('error', (e) => {
         console.error(e);
     });
-    req.end();
 }
 
 function openfreecases() {
-    opencase('3');
-    opencase('62');
+    opencase(3);
+    opencase(62);
 }
 
 openfreecases();
