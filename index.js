a95db3ca0ab7403634d9ab16247b49a31c72c73c
motzmartin <94078957+motzmartin@users.noreply.github.com>
2022-01-19 13:51:23 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const https = require('https');
+const https = require("https");
+const schedule = require("node-schedule");
 
-const token = 'hz7z9bvx8xcsko8sko00s0wo0kcs80k';
+const token = "hz7z9bvx8xcsko8sko00s0wo0kcs80k";
 
 function opencase(id) {
-    const url = `https://csgocases.com/api.php/case/open?id=${id}&number=1&test=false`;
+    const url = "https://csgocases.com/api.php/case/open?id=" + id + "&number=1&test=false";
     const options = {
         headers: {
-            'cookie': `sfRemember=${token}`
+            "cookie": "sfRemember=" + token
         }
     };
     https.get(url, options, (res) => {
-        let data = '';
-        res.on('data', (d) => {
+        let data = "";
+        res.on("data", (d) => {
             data += d;
         });
-        res.on('end', () => {
-            console.log(JSON.parse(data));
+        res.on("end", () => {
+            console.log(data);
         });
-    }).on('error', (e) => {
+    }).on("error", (e) => {
         console.error(e);
     });
 }
 
-function openfreecases() {
+schedule.scheduleJob("0 18 * * *", () => {
     opencase(3);
     opencase(62);
-}
-
-openfreecases();
-setInterval(openfreecases, (24 * 3600 + 10) * 1000);
+});
