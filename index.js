0956425f26560fda5156ff80bf15d3fbeb599312
motzmartin <94078957+motzmartin@users.noreply.github.com>
2022-01-19 14:11:26 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 const https = require("https");
 const schedule = require("node-schedule");
+const nodemailer = require("nodemailer");
 
 const token = "hz7z9bvx8xcsko8sko00s0wo0kcs80k";
 
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "martinmotzcontact@gmail.com",
+        pass: "sV7mUcG584"
+    }
+});
+
 function opencase(id) {
-    const url = "https://csgocases.com/api.php/case/open?id=" + id + "&number=1&test=false";
-    const options = {
-        headers: {
-            "cookie": "sfRemember=" + token
-        }
+    return new Promise((resolve, reject) => {
+        const url = "https://csgocases.com/api.php/case/open?id=" + id + "&number=1&test=false";
+        const options = {
+            headers: {
+                "cookie": "sfRemember=" + token
+            }
+        };
+        https.get(url, options, (res) => {
+            resolve(res.statusMessage);
+        }).on("error", (err) => {
+            reject(err);
+        });
+    });
+}
+
+async function openfreecases() {
+    const case3 = await opencase(3);
+    const case62 = await opencase(62);
+    const mailoptions = {
+        from: "martinmotzcontact@gmail.com",
+        to: "martinmotzcontact@gmail.com",
+        subject: "csgocasesfreecases",
+        text: "case 3 - " + case3 + "\ncase 62 - " + case62
     };
-    https.get(url, options, (res) => {
-        let data = "";
-        res.on("data", (d) => {
-            data += d;
-        });
-        res.on("end", () => {
-            console.log(data);
-        });
-    }).on("error", (e) => {
-        console.error(e);
+    transporter.sendMail(mailoptions, (err, info) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(info);
+        }
     });
 }
 
 schedule.scheduleJob("0 18 * * *", () => {
-    opencase(3);
-    opencase(62);
+    openfreecases();
 });
