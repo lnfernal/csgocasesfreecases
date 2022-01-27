5f1b75d130bb4715403f71894f5093ac4cb8b191
motzmartin <94078957+motzmartin@users.noreply.github.com>
2022-01-27 18:33:23 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,37 +13,34 @@ const transporter = nodemailer.createTransport({
 });
 
 function opencase(id) {
-    return new Promise((resolve, reject) => {
-        const url = "https://csgocases.com/api.php/case/open?id=" + id + "&number=1&test=false";
-        const options = {
-            headers: {
-                "cookie": "sfRemember=" + token
-            }
+    const url = `https://csgocases.com/api.php/case/open?id=${id}&number=1&test=false`;
+    const options = {
+        headers: {
+            "cookie": `sfRemember=${token}`
+        }
+    };
+    https.get(url, options, (res) => {
+        const mailoptions = {
+            from: "martinmotzcontact@gmail.com",
+            to: "martinmotzcontact@gmail.com",
+            subject: `case ${id} - csgocases`,
+            text: `${res.statusCode} - ${res.statusMessage}`
         };
-        https.get(url, options, (res) => {
-            resolve(res.statusMessage);
-        }).on("error", (err) => {
-            reject(err);
+        transporter.sendMail(mailoptions, (err, info) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(info);
+            }
         });
+    }).on("error", (err) => {
+        console.log(err);
     });
 }
 
-async function openfreecases() {
-    const case3 = await opencase(3);
-    const case62 = await opencase(62);
-    const mailoptions = {
-        from: "martinmotzcontact@gmail.com",
-        to: "martinmotzcontact@gmail.com",
-        subject: "csgocasesfreecases",
-        text: "case 3 - " + case3 + "\ncase 62 - " + case62
-    };
-    transporter.sendMail(mailoptions, (err, info) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(info);
-        }
-    });
+function openfreecases() {
+    opencase(3);
+    opencase(62);
 }
 
 schedule.scheduleJob("0 18 * * *", () => {
