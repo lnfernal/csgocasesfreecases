9edfeb7ec2b0c11a43656723323a31aa2448410e
motzmartin <94078957+motzmartin@users.noreply.github.com>
2022-01-31 19:03:03 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 const https = require("https");
-const nodemailer = require("nodemailer");
 
 const token = "hz7z9bvx8xcsko8sko00s0wo0kcs80k";
 
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: "martinmotzcontact@gmail.com",
-        pass: "sV7mUcG584"
-    }
-});
-
-const opencase = (id) => new Promise((resolve, reject) => {
+function opencase(id) {
     const url = `https://csgocases.com/api.php/case/open?id=${id}&number=1&test=false`;
     const options = {
         headers: {
@@ -19,28 +10,16 @@ const opencase = (id) => new Promise((resolve, reject) => {
         }
     };
     https.get(url, options, (res) => {
-        resolve(res.statusCode);
+        console.log(`case ${id}: ${res.statusCode} - ${res.statusMessage}`);
     }).on("error", (err) => {
-        reject(err);
-    });
-});
-
-async function openfreecases() {
-    let code3 = await opencase("3");
-    let code62 = await opencase("62");
-    const mailoptions = {
-        from: "martinmotzcontact@gmail.com",
-        to: "martinmotzcontact@gmail.com",
-        subject: `csgocases`,
-        text: `case 3 - ${code3}\ncase 62 - ${code62}`
-    };
-    transporter.sendMail(mailoptions, (err, info) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(info);
-        }
+        console.error(err);
     });
 }
 
+function openfreecases() {
+    opencase("3");
+    opencase("62");
+}
+
+openfreecases();
 setInterval(openfreecases, (3600 * 24 + 10) * 1000);
